612a1ad21d949f8b6d9d05d16a243579ace04e92
DumbassIdiotgrape <73518925+DumbassIdiotgrape@users.noreply.github.com>
2020-10-29 16:00:10 +0500

    `

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ bot.on('message', message => {
             message.channel.send('sarosh xD and || ali abbass ||')
             break;
         case 'clear':
-            if (message.member.roles.cache.some(role => role.name === 'Owners'))
-                if (!args[1]) return message.reply('Error please definde second arg')
+            if (message.member.hasPermission("ADMINISTRATOR"))
+                message.channel.send('You need admin rights to execute this command')
+            else
+            if (!args[1]) return message.reply('Error please definde second arg')
             message.channel.bulkDelete(args[1]);
 
             break;
